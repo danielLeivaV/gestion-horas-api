1f720a817aaad2214a8f8d8ca863f294223fc0bf
danielLeivaV <ignacioleiva79@gmail.com>
2025-08-29 14:25:57 -0300

    Initial commit

---
+++ src/test/java/com/capimonte/gestion_horas_api/GestionHorasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capimonte.gestion_horas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionHorasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
