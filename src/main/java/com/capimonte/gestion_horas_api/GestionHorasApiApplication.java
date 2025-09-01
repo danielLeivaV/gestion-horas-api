b3f45ec4d6baa18c47d32c261cfedebd735d5b00
Daniel Leiva <ignacioleiva79@gmail.com>
2025-08-31 22:13:28 -0300

    Proyecto iniciado con HelloController y probado con http hello-test.http

---
+++ src/main/java/com/capimonte/gestion_horas_api/GestionHorasApiApplication.java
@@ -0,0 +1,13 @@
+package com.capimonte.gestion_horas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionHorasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionHorasApiApplication.class, args);
+	}
+
+}
